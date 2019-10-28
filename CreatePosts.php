9a0136049fa960de5eb780a2e07790292a84bfa7
Dalton Yoder <dyoder97@gmail.com>
2019-10-27 23:09:35 -0500

    Wrote CreatePosts.php

---
+++ CreatePosts.php
@@ -0,0 +1,28 @@
+<?php
+
+$mysqli = new mysqli("mysql.eecs.ku.edu", "dyoder97", "partytime", "dyoder97");
+
+//Check Connection
+if($mysqli->connect_errno) {
+  printf("Connection failed: %s\n", $mysqli->connect_error);
+  exit();
+}
+$username = $_POST["user"];
+$mypost = $_POST["textpost"];
+
+$userQuery = "SELECT * FROM Users WHERE user_id='$username'";
+$userResult = $mysqli->query($query);
+
+if($userResults->num_rows == 0) {
+  echo "Sorry, that account does not exist!<br>";
+  echo "<a href=\"CreateUser.html\">Create a new Account</a><br>";
+  echo "I'm actually bad at typing in my username, go <a href=\"CreatePosts.html\">back</a> to posting.";
+} else {
+  $newPost = "INSERT INTO Posts (my_post, user_id)
+              VALUES ('$mypost', '$username')";
+  $mysqli->query($newPost);
+  echo "Post Created, <a href\"CreatePosts.html\">go back</a> to see it!<br>";
+}
+
+$mysqli->close();
+ ?>
