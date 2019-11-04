ed9474ee1261ca25ee8a10e9b852949a88abafa3
Dalton Yoder <d510y514@ku.edu>
2019-11-04 14:19:52 -0600

    Made ViewUsers into a table.

--- ViewUsers.php
+++ ViewUsers.php
@@ -6,17 +6,22 @@ if($mysqli->connect_errno) {
   printf("Connection failed: %s\n", $mysqli->connect_error);
   exit();
 }
+echo "<a href=\"AdminHome.html\">Admin Home</a><br><br>";
 
 $query = "SELECT user_id FROM Users";
 $result = $mysqli->query($query);
-
+echo "<table>
+      <tr>
+      <th>Username</th>
+      </tr>";
 if($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
-    echo "Username: " . $row["user_id"] . "<br>";
+    echo "<tr><td>" . $row["user_id"] . "</td></tr>";
   }
 } else {
-  echo "0 results";
+  echo "<tr><td>0 results</td></tr>";
 }
+echo "</table>";
 
 $mysqli->close();
 ?>
